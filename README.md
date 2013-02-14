e095bfb05d151fc4ec3254be81b7bc97ff7d4c0a
Eric Li <li.zhe.hua@gmail.com>
2013-02-14 00:37:48 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# stupid-regex-parser
+
+The best project ever.
+
+## Getting Started
+Install the module with: `npm install stupid-regex-parser`
+
+```javascript
+var stupid_regex_parser = require('stupid-regex-parser');
+stupid_regex_parser.awesome(); // "awesome"
+```
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [grunt](https://github.com/cowboy/grunt).
+
+## Release History
+_(Nothing yet)_
+
+## License
+Copyright (c) 2013 Eric Li  
+Licensed under the MIT license.
---
+++ main.js
@@ -0,0 +1,37 @@
+/*
+ * stupid-regex-parser
+ * https://github.com/tJener/stupid-regex-parser
+ *
+ * Copyright (c) 2013 Eric Li
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var requirejs = require('requirejs');
+
+requirejs.config({
+  baseUrl: 'lib',
+  nodeRequire: require
+});
+
+requirejs([ 'lexer' ], function( Lexer ) {
+  var lexer = new Lexer();
+  lexer.setDefinitions([
+    { name: 'char',                re: '([A-Za-z])'    },
+    { name: 'wildcard',            re: '(\\.)'         },
+    { name: 'kleene',              re: '(\\*)'         },
+    { name: 'plus',                re: '(\\+)'         },
+    { name: 'alternation',         re: '(\\|)'         },
+    { name: 'group_start',         re: '(\\()'         },
+    { name: 'group_end',           re: '(\\))'         },
+    { name: 'negated_class_start', re: '(\\[\\^)'      },
+    { name: 'class_start',         re: '(\\[)(?=[^^])' },
+    { name: 'class_end',           re: '(\\])'         },
+    { name: 'negate_class',        re: '(\\^)'         },
+    { name: 'back_ref',            re: '\\\\(\\d)'     }
+  ]);
+
+  console.log( lexer.tokenize( '.*SE.*UE.*' ));
+  console.log( lexer.tokenize( '([^MC]|MM|CC)*' ));
+});
